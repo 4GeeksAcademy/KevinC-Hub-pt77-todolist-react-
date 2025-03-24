a6d5bf953f3b189c4d96d09a93e56f27813af9c0
KevinC-hub <canchonkevin@gmail.com>
2025-03-24 23:26:55 +0000

    Done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,45 +1,50 @@
 import React, { useState } from "react";
 
 //create your first component
+
 const Home = () => {
-	const [inputValue, setInputValue] = useSate("");
-	const [todos, setTodos] = useState([]);
+	const [ inputValue, setInputValue ] = useState("")
+	const [ todos, setTodos ] = useState([])
+
+
 	return (
 		<div className="container">
 			<h1>My ToDo List</h1>
 			<ul>
 				<li>
-					<input
+					<input 
 						type="text"
 						onChange={(e) => setInputValue(e.target.value)}
 						value={inputValue}
-						onKeyDown={(e) =>
+						onKeyDown={(e) => {
 							if (e.key === "Enter") {
-							setTodos(todos.concat(inputValue));
-							setInputValue("");
-						}
+								setTodos(todos.concat(inputValue));
+								setInputValue("");
+							}
 						}}
 						placeholder="What do you need to do?"></input>
 				</li>
-				{todos.map((item, index)) => {
+				{todos.map((item, index) => (
 					<li>
-						{item}{""}
+						{item}{""} 
 						<i 
-							class="fa-solid fa-x" 
-							onClick={() =>
+							class="fa-regular fa-x"
+							onClick={()=> 
 								setTodos(
 									todos.filter(
-										(t, currentIndex) =>
-											index != currentIndex
+										(t, currentIndex) => 
+											index != currentIndex 
+									)
 								)
-							)
 							}></i>
 					</li>
-				}
+				))}
 			</ul>
-			<div>{todos.length} tasks remaining</div>
-		</div >
+			<div>{todos.length} tasks</div>
+		</div>
 	);
 };
+
+
 
 export default Home;
